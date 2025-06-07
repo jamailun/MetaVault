62593e58c19eb7f72b9098e948e173e0cff82f65
timothe.rosaz <jamailun@laposte.net>
2025-06-07 13:35:21 +0200

    Removed debug observer

--- src/main/java/fr/jamailun/metaVault/MetaVault.java
+++ src/main/java/fr/jamailun/metaVault/MetaVault.java
@@ -1,7 +1,6 @@
 package fr.jamailun.metaVault;
 
 import fr.jamailun.metaVault.commands.MetaVaultCommand;
-import fr.jamailun.metaVault.observer.Observer;
 import fr.jamailun.metaVault.storage.Stoppable;
 import fr.jamailun.metaVault.storage.Storage;
 import fr.jamailun.metaVault.storage.StorageProviderFactory;
@@ -9,14 +8,12 @@ import fr.jamailun.metaVault.storage.exception.StorageInitException;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.UUID;
 import java.util.logging.Level;
 
 /**
@@ -57,21 +54,6 @@ public final class MetaVault extends JavaPlugin {
     public void onEnable() {
         // Register command
         new MetaVaultCommand(storage, initError);
-
-        // Debug observer
-        if(storage != null) {
-            Plugin that = this;
-            storage.observe(new Observer() {
-                @Override
-                public @NotNull Plugin getPlugin() {
-                    return that;
-                }
-                @Override
-                public void onValueChanged(@NotNull UUID uuid, @NotNull String key, @Nullable String value) {
-                    info("VALUE CHANGED : " + uuid + "[" + key + "] = " + (value==null?"null":"(" + value + ")"));
-                }
-            });
-        }
     }
 
     @Override
